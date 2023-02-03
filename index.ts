38f1f7b8aa7bf2b77ff73a539bd5605647d86244
sosso21 <sofianetop21.st@gmail.com>
2023-02-03 19:56:08 +0100

    :sparkles: (init): creation

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const commands = [
     description: "CreateRole role",
   },
 ];
-const token = process.env.DISCORD_API_BOT_TOKEN as any;
+const token = process.env.DISCORD_BOT_TOKEN as any;
 
 const rest = new REST({ version: "10" }).setToken(token);
 
@@ -22,7 +22,7 @@ const rest = new REST({ version: "10" }).setToken(token);
     console.log("Started refreshing application (/) commands.");
 
     await rest.put(
-      Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID as any),
+      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID as any),
       { body: commands }
     );
 
@@ -43,51 +43,6 @@ const rest = new REST({ version: "10" }).setToken(token);
 
       if (interaction.commandName === "CreateRole") {
         //--
-        const rolesArray = [
-          "patron 💼",
-          "direction 🏢",
-          "administrateur 🔧",
-          "moderateur 🛡️",
-          "developpeur 💻",
-          "follower 👥",
-          "chef_margineau 🍔",
-          "bot 🤖",
-          "realisateur 🎬",
-          "senariste 📝",
-          "story_bording 📖",
-          "monteur 🔪",
-          "acteur 🎭",
-          "montion_design 🔊",
-          "male 👨",
-          "female 👩",
-          "europ 🇪🇺",
-          "afrique 🇿🇦",
-          "asie 🇨🇳",
-          "america 🇺🇸",
-          "oceanie🌏",
-        ];
-
-        const guild = interaction.guild;
-
-        const roles = guild?.roles;
-
-        const cache = roles?.cache;
-
-        for (let index = 0; index < rolesArray.length; index++) {
-          const roleName = rolesArray[index];
-
-          if (!cache?.find((role) => role.name == roleName)) {
-            const role = await roles?.create({
-              name: roleName,
-              permissions: [],
-              reason: "we needed a role for Super Cool People",
-            });
-          }
-        }
-        console.log(
-          "cache:",
-          cache?.map((i) => i.name)
-        );
 
         await interaction.reply("C regl");
       }
